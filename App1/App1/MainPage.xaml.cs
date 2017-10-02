cb99d6d79347299bbdbbfc7f92228f5ca1a06fb0
JoaoRacedo <racedo@uninorte.edu.co>
2017-10-02 17:21:16 -0500

    first commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+            //InitializeComponent();
+
+            var label = new Label { Text = "Hola, presiona el boton", Margin = new Thickness(20) };
+            var boton = new Button { Text = "Boton",  Margin = new Thickness(0, 20, 15, 5) };
+            boton.Clicked += ElEventoClick;
+
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    label,
+                    boton
+                }
+            };
+        }
+
+
+        void ElEventoClick(object sender, EventArgs e) {
+            DisplayAlert("Hola", "Click", "ok");
+        }
+
+    }
+}
